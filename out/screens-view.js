368626f552885cdf5bdec913d4c8662e81ef158c
Zakai Hamilton <zakaihamilton@gmail.com>
2019-07-09 23:51:12 +0300

    render component on server

--- out/screens-view.js
+++ out/screens-view.js
@@ -11,30 +11,11 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const screens_js_1 = __importDefault(require("screens-js"));
-const server_1 = require("react-dom/server");
-const react_1 = __importDefault(require("react"));
 screens_js_1.default.startup().then(async () => {
-    await screens_js_1.default.import("packages", __dirname, async (path) => {
-        console.log(path);
-        await Promise.resolve().then(() => __importStar(require("./" + path)));
-    });
+    await screens_js_1.default.import("packages", __dirname, async (path) => await Promise.resolve().then(() => __importStar(require("./" + path))));
     await screens_js_1.default.init();
     console.log("init complete");
-    screens_js_1.default.CoreHttp.register(/^\/$/, (req, resp) => {
-        let headers = {
-            "Content-Type": "text/html",
-            "Access-Control-Allow-Methods": "*",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "*"
-        };
-        resp.writeHead(200, headers);
-        resp.write("<!DOCTYPE html><html><head><title>My Page</title></head><body>");
-        let Example = screens_js_1.default.Example;
-        const stream = server_1.renderToNodeStream(react_1.default.createElement(Example, null));
-        stream.pipe(resp, { end: false });
-        stream.on('end', () => {
-            resp.write("</div></body></html>");
-            resp.end();
-        });
+    screens_js_1.default.CoreHttp.register(/^\/$/, async function (req, resp) {
+        await this.me.UIRender.component(screens_js_1.default.Example);
     });
 });
--- screens-view.tsx
+++ screens-view.tsx
@@ -1,30 +1,10 @@
 import screens from "screens-js"
-import { renderToNodeStream } from 'react-dom/server';
-import React, { useState } from 'react';
 
 screens.startup().then(async () => {
-    await screens.import("packages", __dirname, async (path: string) => {
-        console.log(path);
-        await import("./" + path);
-    });
+    await screens.import("packages", __dirname, async (path: string) => await import("./" + path));
     await screens.init();
     console.log("init complete");
-    screens.CoreHttp.register(/^\/$/, (req: any, resp: any) => {
-        let headers = {
-            "Content-Type": "text/html",
-            "Access-Control-Allow-Methods": "*",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "*"
-        };
-
-        resp.writeHead(200, headers);
-        resp.write("<!DOCTYPE html><html><head><title>My Page</title></head><body>");
-        let Example = screens.Example;
-        const stream = renderToNodeStream(<Example />);
-        stream.pipe(resp, { end: false });
-        stream.on('end', () => {
-            resp.write("</div></body></html>");
-            resp.end();
-        });
+    screens.CoreHttp.register(/^\/$/, async function (this: any, req: any, resp: any) {
+        await this.me.UIRender.component(screens.Example);
     });
 });
